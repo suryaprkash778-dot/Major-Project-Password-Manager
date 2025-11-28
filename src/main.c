14b06b98087a3ee8462ab6fd737a8838d36984e0
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2025-11-28 09:56:54 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main() {
     load_from_file(); // load existing credentials at startup
 
     do {
-        printf("\n=== Password Manager (Text File Storage) ===\n");
+        printf("\n=== Password Manager (Plain Text Storage) ===\n");
         printf("1. Add Credential\n");
         printf("2. View All Credentials\n");
         printf("3. Search by Website\n");
@@ -24,6 +24,6 @@ int main() {
             default: printf("Invalid choice. Try again.\n");
         }
     } while (choice != 5);
+
     return 0;
 }
-
